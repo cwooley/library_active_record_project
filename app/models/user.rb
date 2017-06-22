b9b4399728f3594db9e4f843472eba0742ca42d3
github username <github email address>
2017-06-22 15:20:32 -0400

    Made other misc changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_many :books, through: :books_user
 
 
-  def check_out_book(book)
-    relation = BooksUser.new(book_id: book.id, user_id: self.id)
+  def check_out_book(book, due_date)
+    relation = BooksUser.create(book_id: book.id, user_id: self.id, due_date: due_date)
   end
 end
